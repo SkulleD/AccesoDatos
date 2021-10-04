ecb66d539fed17d35853bbdc8efa9b490147a39c
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-10-04 18:20:07 +0200

    Add files via upload

--- Tema1/src/tema1/Practica.java
+++ Tema1/src/tema1/Practica.java
@@ -3,27 +3,26 @@ package tema1;
 import java.io.File;
 
 public class Practica {
-	String ruta = "C:\\Users\\AlvaroVila\\eclipse-workspace\\Tema1\\src\\tema1\\prueba";
-	File archivo = new File(ruta);
-
 	public static void main(String[] args) {
 		Practica practica = new Practica();
-		practica.muestraCosas();
+		String ruta = "C:\\Users\\AlvaroVila\\eclipse-workspace\\Tema1\\src\\tema1\\prueba";
+		File archivo = new File(ruta);
+		
+		practica.muestraCosas(archivo);
 	}
 
-	public void muestraCosas() {
-		File[] lista = archivo.listFiles();
-
+	public void muestraCosas(File archivo) {
 		if (archivo.exists()) {
+			File[] lista = archivo.listFiles();
 			for (File file : lista) {
 				if (file.isFile()) {
-					System.out.println("-file-");
+					System.out.print("-file:-");
 					System.out.println(file.getName());
 				}
 			}
 			for (File file : lista) {
 				if (file.isDirectory()) {
-					System.out.println("-folder-");
+					System.out.print("-folder:-");
 					System.out.println(file.getName());
 				}
 			}
