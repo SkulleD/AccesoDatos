cd0937e25452fbfb8b9053ac2756410e7196171a
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-10-07 13:49:36 +0200

    Add files via upload

--- Ficheros/src/tema1/Ficheros_ej7.java
+++ Ficheros/src/tema1/Ficheros_ej7.java
@@ -1,16 +1,49 @@
 package tema1;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Collection;
+import java.util.Scanner;
 
 public class Ficheros_ej7 {
+	int numLineas = 0;
+	int numPalabras = 0;
 
-	public <T> void ordenarCharas(File file, Collection<T> collection) {
-		
+	public void menu() {
+		int menu = 0;
+
+		switch (menu) {
+
+		}
+	}
+
+	public <T> void contador(File file) throws FileNotFoundException {
+		try (Scanner sc = new Scanner(file)) {
+			while (sc.hasNextLine()) {
+				sc.nextLine();
+				numLineas++;
+			}
+		}
+
+		try (Scanner sc2 = new Scanner(file)) {
+			while (sc2.hasNext()) {
+				sc2.next();
+				numPalabras++;
+			}
+		}
+
+		System.out.println("Líneas: " + numLineas + "\nPalabras: " + numPalabras);
 	}
 	
-	public static void main(String[] args) {
+	public void opcion_A() {
 		
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Ficheros_ej7 ej7 = new Ficheros_ej7();
+		File file = new File("C:\\Users\\AlvaroVila\\eclipse-workspace\\Ficheros\\Ejercicio7\\Texto_ej7.txt");
+
+		ej7.contador(file);
 	}
 
 }
---
+++ Ficheros/src/tema1/Ficheros_ej8.java
@@ -0,0 +1,9 @@
+package tema1;
+
+public class Ficheros_ej8 {
+
+	public static void main(String[] args) {
+		System.out.println("adffdsfd");
+	}
+
+}
