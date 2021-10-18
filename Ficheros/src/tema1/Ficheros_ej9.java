a115a2895d2c94111f639b36993e8eb48b999890
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-10-18 19:58:56 +0200

    Add files via upload

--- Ficheros/src/tema1/Ficheros_ej9.java
+++ Ficheros/src/tema1/Ficheros_ej9.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class Ficheros_ej9 {
 	Alumno alumno = new Alumno();
-	Ficheros_ej9 ej9 = new Ficheros_ej9();
+	static Ficheros_ej9 ej9 = new Ficheros_ej9();
 	ArrayList<Alumno> listado = new ArrayList<>();
 	
 	public void nuevoAlumno(int codigo, String nombre, double altura) {
@@ -30,6 +30,12 @@ public class Ficheros_ej9 {
 		
 	}
 	
+	public void muestraAlumnos() {
+		for (Alumno alumno : listado) {
+			System.out.printf("\nCódigo: %d\nNombre: %s\nAltura: %.2f", alumno.getCodigo(), alumno.getNombre(), alumno.getAltura());
+		}
+	}
+	
 	public void anadirEnArchivo(File file) {
 		try (FileOutputStream output = new FileOutputStream(file)) {
 			int c = 0;
@@ -49,6 +55,10 @@ public class Ficheros_ej9 {
 	public static void main(String[] args) {
 		File fileAlumnos = new File("C:\\Users\\AlvaroVila\\eclipse-workspace\\Ficheros\\Ejercicio9\\alumnos.dat");
 		
-		ej9.nuevoAlumno(42, "Álvaro", 1.82);
+		ej9.nuevoAlumno(21, "Alumno1", 1.82);
+		ej9.nuevoAlumno(32, "Alumno2", 1.56);
+		ej9.nuevoAlumno(43, "Alumno3", 1.75);
+		ej9.nuevoAlumno(54, "Alumno4", 1.89);
+		ej9.muestraAlumnos();
 	}
 }
