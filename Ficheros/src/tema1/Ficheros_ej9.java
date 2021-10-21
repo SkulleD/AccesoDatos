aa15acddceaa907c7aaf8e9f17416ef3449b35cd
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-10-21 13:51:51 +0200

    Add files via upload

--- Ficheros/src/tema1/Ficheros_ej9.java
+++ Ficheros/src/tema1/Ficheros_ej9.java
@@ -12,105 +12,77 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Ficheros_ej9 {
-	Alumno alumno;
-	static Ficheros_ej9 ej9 = new Ficheros_ej9();
-    File fileAlumnos = new File("C:\\Users\\AlvaroVila\\eclipse-workspace\\Ficheros\\Ejercicio9\\alumnos.txt");
-    File fileAux = new File("C:\\Users\\AlvaroVila\\eclipse-workspace\\Ficheros\\Ejercicio9\\auxiliar.txt");
 	String cadena = "";
+	static Alumno alumno = new Alumno();
+	boolean existe = false;
 
-	public String nuevoAlumno(int codigo, String nombre, float altura, ArrayList<Alumno> listado) throws FileNotFoundException, IOException {
+	public void nuevoAlumno(File file, Alumno alumno) throws IOException {
+		if (siExiste(alumno.getCodigo())) {
+			try (DataOutputStream output = new DataOutputStream(new FileOutputStream(file, true))) {
+				output.writeInt(alumno.getCodigo());
+				output.writeUTF(alumno.getNombre());
+				output.writeFloat(alumno.getAltura());
+			} catch (EOFException e) {
+				System.out.println("Fin de fichero");
+			} catch (IOException e) {
 
-			//cadena = "(!) Ya existe un alumno con ese código.";
-	
-
-			alumno.setCodigo(codigo);
-			alumno.setNombre(nombre);
-			alumno.setAltura(altura);
-			anadirEnArchivo(alumno.getCodigo(), fileAlumnos, listado);
-
-		return cadena;
+			}
+		} else {
+			System.out.println("Ya existe un alumno con ese código");
+		}
 	}
 
-	public void consultarAlumnos(File file, ArrayList<Alumno> listado) {
+	public void consultarAlumnos(File file) {
 		try (DataInputStream input = new DataInputStream(new FileInputStream(file))) {
 			int codigo;
 			String nombre;
 			float altura;
-
+			
 			while (true) {
 				codigo = input.readInt();
 				nombre = input.readUTF();
 				altura = input.readFloat();
-				System.out.printf("Código: %d\nNombre: %s\nAltura: %f", codigo, nombre, altura);
+				System.out.printf("Código: %d\nNombre: %s\nAltura: %.2f", codigo, nombre, altura);
+				System.out.println("\n");
 			}
-
 		} catch (FileNotFoundException e) {
 
 		} catch (IOException e) {
 
 		}
-
-		for (Alumno alumno : listado) {
-			System.out.printf("\nCódigo: %d\nNombre: %s\nAltura: %.2f\n", alumno.getCodigo(), alumno.getNombre(),
-					alumno.getAltura());
-		}
 	}
 
-	public String modificarAlumnos(int codigo, ArrayList<Alumno> listado) {
-		if (siExiste(codigo, listado)) {
+	public void modificarAlumnos() {
 
+	}
+
+	public void borrarAlumnos() {
+
+	}
+	
+	public boolean siExiste(int codigo) {
+		if (alumno.getCodigo() == codigo) {
+			existe = true;
 		} else {
-			cadena = "(!) No existe ningún alumno con ese código.";
-			return cadena;
+			existe = false;
 		}
-		return cadena;
-	}
-
-	public String borrarAlumnos(int codigo, ArrayList<Alumno> listado) {
-		if (siExiste(codigo, listado)) {
-			//listado.remove(alumno);
-
-			cadena = "Se ha borrado el alumno";
-		} else {
-			cadena = "(!) No existe ningún alumno con ese código.";
-			return cadena;
-		}
-		return cadena;
-	}
-
-	public boolean siExiste(int codigo, ArrayList<Alumno> listado) {
-		boolean existe = false;
-
-		for (Alumno alumno : listado) {
-			if (alumno.getCodigo() == codigo) {
-				existe = true;
-			}
-		}
+		
 		return existe;
 	}
 
-	public void anadirEnArchivo(int codigo, File file, ArrayList<Alumno> listado) throws FileNotFoundException, IOException {
-		//if (!siExiste(codigo, listado)) {
-			try (DataOutputStream output = new DataOutputStream(new FileOutputStream(file))) {
-				for (Alumno alumno : listado) {
-					output.writeInt(alumno.getCodigo());
-					output.writeUTF(alumno.getNombre());
-					output.writeFloat(alumno.getAltura());
-				}
-			} catch (EOFException e) {
-				System.out.println("Fin de fichero");
-		//	}
-		}
-	}
-
 	public static void main(String[] args) throws IOException {
-		ArrayList<Alumno> listado = new ArrayList<>();
+		File fileAlumnos = new File("C:\\Users\\AlvaroVila\\eclipse-workspace\\Ficheros\\Ejercicio9\\alumnos.dat");
+		File fileAux = new File("C:\\Users\\AlvaroVila\\eclipse-workspace\\Ficheros\\Ejercicio9\\auxiliar.dat");
+		Ficheros_ej9 ej9 = new Ficheros_ej9();
 
 		for (int i = 1; i <= 5; i++) {
-			listado.add(new Alumno(i, "Alumno " + i, i + 100));
-			ej9.nuevoAlumno(i, "Alumno " + i, i, listado);
+			alumno.setCodigo(i);
+			alumno.setNombre("Nombre" + i);
+			alumno.setAltura(i + 100);
+			ej9.nuevoAlumno(fileAlumnos, alumno);
+			
 		}
-		//ej9.nuevoAlumno(2, "Alumno " + 2, 175, listado);
-		ej9.consultarAlumnos(ej9.fileAlumnos, listado);
+		ej9.consultarAlumnos(fileAlumnos);
+		
 	}
 }
