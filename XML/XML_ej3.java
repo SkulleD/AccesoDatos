07229c677ae9bc7c59817560508cbeb941a3c921
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-11-27 18:19:22 +0100

    Add files via upload

--- XML/XML_ej3.java
+++ XML/XML_ej3.java
@@ -1,38 +1,36 @@
-import javax.xml.parsers.DocumentBuilderFactory;
+package xmlpackage;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class XML_ej3 {
-	public void getPeliculas(Document doc) {
-		NodeList titulos = doc.getElementsByTagName("titulo");
-		NodeList directores;
-		NodeList nombres;
-		NodeList apellidos;
+	public static void muestraDirectores(Document doc) {
 		Element padre;
-		
+		NodeList titulos = doc.getElementsByTagName("titulo");
+		NodeList directores, nombres, apellidos;
+
 		for (int i = 0; i < titulos.getLength(); i++) {
-			padre = (Element)titulos.item(i).getParentNode();
-			System.out.println("-----------------");
-			System.out.printf("Título: %s \n", titulos.item(i).getFirstChild().getNodeValue());
-			
+			padre = (Element) titulos.item(i).getParentNode();
+			System.out.println("----------");
+			System.out.println(
+					"Título: " + titulos.item(i).getFirstChild().getNodeValue() + " | Género: " + padre.getAttribute("genero"));
+
 			directores = padre.getElementsByTagName("director");
 			nombres = padre.getElementsByTagName("nombre");
 			apellidos = padre.getElementsByTagName("apellido");
-			
+
 			for (int j = 0; j < directores.getLength(); j++) {
-				System.out.printf("Director/a: %s %s\n", nombres.item(j).getFirstChild().getNodeValue(), apellidos.item(j).getFirstChild().getNodeValue());
+				System.out.println("Nombre: " + nombres.item(j).getFirstChild().getNodeValue());
+				System.out.println("Apellido/s: " + apellidos.item(j).getFirstChild().getNodeValue());
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		XML_ej3 dom = new XML_ej3();
-		XML_ej1 dom1 = new XML_ej1();
-		String ruta = "file:///C://Users//AlvaroVila//Desktop//CLASE//AD - Acceso a Datos//Práctica//peliculas.xml";
-		Document documento = dom1.creaArbol(ruta);
-		dom.getPeliculas(documento);
+		XML_ej1 ej1 = new XML_ej1();
+		String ruta = "C:\\peliculas.xml";
+		Document doc = ej1.creaArbol(ruta);
+		muestraDirectores(doc);
 	}
 }
